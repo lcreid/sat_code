02378909636d549927350a919d0662cfdc1ee329
Bill-Gray <pluto@projectpluto.com>
2017-03-26 12:18:11 -0400

    Revised to output perigee,  apogee,  orbital period data.  Just needed a simple utility for that purpose.

--- test_out.cpp
+++ test_out.cpp
@@ -1,17 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 #include "norad.h"
 #include "norad_in.h"
 
 /* Example code to add BSTAR data using Ted Molczan's method.  It just
    reads in TLEs,  computes BSTAR if possible,  then writes out the
-   resulting modified TLE.                                           */
+   resulting modified TLE.
 
-int main( const int argc, const char **argv)
+   Add the '-v' (verbose) switch,  and it also writes out the orbital
+   period and perigee/apogee distances.  Eventually,  I'll probably
+   set it up to dump other data that are not immediately obvious
+   just by looking at the TLEs... */
+
+int main( int argc, const char **argv)
 {
-   FILE *ifile = fopen( (argc == 1 ? "all_tle.txt" : argv[1]), "rb");
+   FILE *ifile;
+   const char *filename;
    char line1[100], line2[100];
+   int verbose = 0;
 
+   if( !strcmp( argv[argc - 1], "-v"))
+      {
+      verbose = 1;
+      argc--;
+      }
+   filename = (argc == 1 ? "all_tle.txt" : argv[1]);
+   ifile = fopen( filename, "rb");
+   if( !ifile)
+      {
+      fprintf( stderr, "Couldn't open '%s': ", filename);
+      perror( "");
+      return( -1);
+      }
    while( fgets( line1, sizeof( line1), ifile))
       if( *line1 == '1' && fgets( line2, sizeof( line2), ifile)
                   && *line2 == '2')
@@ -29,6 +50,17 @@ int main( const int argc, const char **argv)
                tle.bstar = tle.xndt2o / (tle.xno * c2 * 1.5);
             write_elements_in_tle_format( obuff, &tle);
             printf( "%s", obuff);
+            if( verbose)
+               {
+               const double a1 = pow(xke / tle.xno, two_thirds);  /* in Earth radii */
+
+               printf( "   Perigee: %.4f km\n",
+                    (a1 * (1. - tle.eo) - 1.) * earth_radius_in_km);
+               printf( "   Apogee: %.4f km\n",
+                    (a1 * (1. + tle.eo) - 1.) * earth_radius_in_km);
+               printf( "   Orbital period: %.4f min\n",
+                    2. * pi / tle.xno);
+               }
             }
          }
       else
